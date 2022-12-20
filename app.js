7f46a32522e990fd064f2e9ff9f0d4a4875b33cc
Azrul Haifan <ahaifan@gmail.com>
2022-12-20 09:58:55 +0700

    Try adding .htaccess

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const host = "127.0.0.1"
+const port = 18127
 
-app.listen(port, () => {
-  console.log(`Voia running on port ${port}`)
+app.listen(port, host, () => {
+  console.log(`Voia running on port http://${host}:${port}`)
 })
 
 // ROUTE DEFINITION
