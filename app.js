d76f4fed33981a8e77a258ec1b5a988111830340
Azrul Haifan <ahaifan@gmail.com>
2022-12-20 11:23:18 +0700

    Try to use unixsoc

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express()
-const host = "127.0.0.1"
-const port = 18127
+const hard_host = "127.0.0.1"
+const hard_port = 18127
 
-app.listen(port, host, () => {
-  console.log(`Voia running on port http://${host}:${port}`)
+const {
+	NODE_ENV,
+	PORT,
+	HOST,
+} = process.env;
+
+app.listen(PORT ? PORT : hard_port, () => {
+  app.currentServer = {
+		host: HOST ? HOST : hard_host,
+		port: PORT ? PORT : hard_port,
+	};
+
+  console.log(`Voia running on port http://${HOST}:${PORT}`)
 })
 
 // ROUTE DEFINITION
