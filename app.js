3c786d8bf6bbb6d4dfa997a16ccf655d07c5a0c8
Azrul Haifan <ahaifan@gmail.com>
2022-12-20 11:37:28 +0700

    Adding .env

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
+require('dotenv').config({path: __dirname + '/.env' })
 const express = require('express')
 const app = express()
 const hard_host = "127.0.0.1"
 const hard_port = 18127
 
-const {
-	NODE_ENV,
-	PORT,
-	HOST,
-} = process.env;
+const host = process.env.HOST || hard_host
+const port = parseInt(process.env.PORT ?? hard_port)
 
-app.listen(PORT ? PORT : hard_port, () => {
-  app.currentServer = {
-		host: HOST ? HOST : hard_host,
-		port: PORT ? PORT : hard_port,
-	};
-
-  console.log(`Voia running on port http://${HOST}:${PORT}`)
+app.listen(port, () => {
+  console.log(`Voia running on port http://${host}:${port}`)
 })
 
 // ROUTE DEFINITION
